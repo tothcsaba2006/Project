8a9892d5dd604fa6dbfeaf30e378388424bda5ac
VBenedek2006 <90269623+VBenedek2006@users.noreply.github.com>
2026-02-19 09:42:40 +0100

    Add files via upload

--- webshop.sql
+++ webshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2026. Feb 04. 13:13
+-- Létrehozás ideje: 2026. Feb 19. 09:40
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -74,8 +74,36 @@ CREATE TABLE `products` (
   `description` text DEFAULT NULL,
   `price` decimal(10,2) NOT NULL,
   `stock` int(11) NOT NULL,
-  `category` varchar(255) DEFAULT NULL
+  `category` varchar(255) DEFAULT NULL,
+  `main_image` varchar(255) DEFAULT NULL,
+  `sizes` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `stock`, `category`, `main_image`, `sizes`) VALUES
+(1, 'Nike Air Force 1 07 Triple White', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 40000.00, 15, 'Sneakerek', '/img/termek1.webp', '40, 41, 42'),
+(2, 'Nike Air Force 1 Black Chunky Rope Laces Beige', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 45000.00, 15, 'Sneakerek', '/img/termek2.webp', '40, 41, 42'),
+(3, 'NIKE AIR FORCE 1 LOW 07 BLACK WHITE ROPE LACES', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 45000.00, 15, 'Sneakerek', '/img/termek3.webp', '40, 41, 42'),
+(4, 'NIKE AIR FORCE 1 LOW 07 WHITE Rose Rope Laces', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 50000.00, 15, 'Sneakerek', '/img/termek4.webp', '40, 41, 42'),
+(5, 'Nike Air Force 1 Low Supreme White', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 55000.00, 15, 'Sneakerek', '/img/termek5.webp', '40, 41, 42'),
+(6, 'Nike Air Force 1 Low Supreme Black', 'A Nike Air Force 1 Low Supreme Black egy ikonikus sneaker a mindennapokra. Az egyszerű fekete borítású megjelenést egy élénk piros Supreme felirat teszi különlegessé...', 55000.00, 15, 'Sneakerek', '/img/termek6.webp', '40, 41, 42'),
+(7, 'Nike Air Force 1 Mid Supreme White', 'A Supreme visszavesz az agresszív márkajelzésből, és helyette egy egyszerű és finom dizájnt választ, az ikonikus piros Box Logót a fehér-fehér Air Force 1 Mid sarkán...', 60000.00, 15, 'Sneakerek', '/img/termek7.webp', '40, 41, 42'),
+(8, 'Nike Air Force 1 Low Off-White MCA University Blue', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 410000.00, 15, 'Sneakerek', '/img/termek8.webp', '40, 41, 42'),
+(9, 'Air Jordan 1 Retro High Og Chicago Lost And Found', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 80000.00, 15, 'Sneakerek', '/img/termek9.webp', '40, 41, 42'),
+(10, 'Air Jordan 1 Retro Low OG SP Travis Scott Canary', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 120000.00, 15, 'Sneakerek', '/img/termek10.webp', '40, 41, 42'),
+(11, 'Air Jordan 1 Retro High Dior', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 1200000.00, 15, 'Sneakerek', '/img/termek11.webp', '40, 41, 42'),
+(12, 'Nike Air Jordan 1 Retro High Mocha', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 180000.00, 15, 'Sneakerek', '/img/termek12.webp', '40, 41, 42'),
+(13, 'Air Jordan 1 Retro High OG Bred Patent', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 80000.00, 15, 'Sneakerek', '/img/termek13.webp', '40, 41, 42'),
+(14, 'Air Jordan 1 Retro High White University Blue Black', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 130000.00, 15, 'Sneakerek', '/img/termek14.webp', '40, 41, 42'),
+(15, 'Air Jordan 1 Retro High OG NRG Not For Resale', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 280000.00, 15, 'Sneakerek', '/img/termek15.webp', '40, 41, 42'),
+(16, 'Adidas Campus 00s Core Black', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 30000.00, 15, 'Sneakerek', '/img/termek16.webp', '40, 41, 42'),
+(17, 'Adidas Campus 00s Crystal White Core Black', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 30000.00, 15, 'Sneakerek', '/img/termek17.webp', '40, 41, 42'),
+(18, 'Adidas Campus 00s Better Scarlet Cloud White', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 30000.00, 15, 'Sneakerek', '/img/termek18.webp', '40, 41, 42'),
+(19, 'UGG Tazz Slipper Sand', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 40000.00, 15, 'Sneakerek', '/img/termek19.webp', '40, 41, 42'),
+(31, 'S Logo Zip Up Hooded Sweatshirt', 'A Nike Air Force 1 07 Triple White sneaker egy igazán egyszerű, de sikkes darab...', 70000.00, 15, 'Ruházat', '/img/termek31.webp', 'S, M, L');
 
 -- --------------------------------------------------------
 
@@ -140,7 +168,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT a táblához `cart`
@@ -152,19 +180,19 @@ ALTER TABLE `cart`
 -- AUTO_INCREMENT a táblához `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT a táblához `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Megkötések a kiírt táblákhoz
